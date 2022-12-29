5be95e5ac5021e3f5e2d086a323938211a0e2b99
root <root@ip-10-250-134-226.ec2.internal>
2022-12-29 13:24:03 +0000

    Adding project files

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,18 +1,26 @@
+
 #!/bin/bash
-while read aws_account
-do
-  rm -f files/*.txt
-  for region in `echo "us-east-1 eu-central-1 ca-central-1 ap-south-1 ap-southeast-1 ap-southeast-2"`
-  do
-    export AWS_REGION=$region
-    export AWS_PROFILE=$aws_account
-    ansible-playbook get_ebs_volumes_with_available_status.yml
-    cat files/*.txt > reports/detached_ebs_$AWS_PROFILE.csv
-    
-    sed -it '/^ *$/d' reports/detached_ebs_$AWS_PROFILE.csv
-    sed -it 's/^ *//' reports/detached_ebs_$AWS_PROFILE.csv
-  done
-  #sed -it '/^ *$/d' reports/discount_tag_$AWS_PROFILE.csv
-  #sed -it '/^ *$/d' reports/discount_tag_$AWS_PROFILE.csv
-done < aws_accounts
+if [ $# -eq 0 ]
+then
+	echo "Usage: ./run_ansible.sh <git_repo> <playbook_name> <optional_parameters>"
+fi
+#docker run -v "${PWD}":/work:ro -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm spy86/ansible:latest ansible-playbook playbook.yml
+git_repo=$1
+git_repo_dir=`echo $git_repo | grep -o "/.*\.git" | sed -n 's/\///p'`
+shift
+playbook_name=$2
+shift
+cd /opt;git clone $git_repo
+## See if git clone is successful
+if [ $? -ne 0 ]
+then
+	cd /opt/$git_repo_dir
+	ansible-vault decrypt config.yml --vault-password-file ~/.ansible_pass
+	sudo docker run --env AWS_SHARED_CREDENTIALS_FILE=/work/.aws/credentials -v "${PWD}":/work:rw -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm ansible_from_ubuntu:latest  ansible-playbook $playbook_name $*
+	cp /opt/*.csv /media
+	git clean -f -d
+else
+	echo "Error in cloning git repo"
+fi
+
 
