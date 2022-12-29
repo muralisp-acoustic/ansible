8d44ed63f7edce332419f0322483cd248de5b26d
muralisp-acoustic <112857287+muralisp-acoustic@users.noreply.github.com>
2022-12-29 16:00:08 +0000

    enabling all the aws accounts

--- run_ansible.sh
+++ run_ansible.sh
@@ -6,18 +6,19 @@ then
 fi
 #docker run -v "${PWD}":/work:ro -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm spy86/ansible:latest ansible-playbook playbook.yml
 git_repo=$1
-git_repo_dir=`echo $git_repo | grep -o "/.*\.git" | sed -n 's/\///p'`
+git_repo_dir=`echo $git_repo | grep -o "/.*\.git" | sed -n -e 's/\///' -e 's/\.git//p'`
 shift
-playbook_name=$2
+playbook_name=$1
 shift
-cd /opt;git clone $git_repo
+git clone $git_repo
 ## See if git clone is successful
-if [ $? -ne 0 ]
+if [ $? -eq 0 ]
 then
-	cd /opt/$git_repo_dir
-	ansible-vault decrypt config.yml --vault-password-file ~/.ansible_pass
-	sudo docker run --env AWS_SHARED_CREDENTIALS_FILE=/work/.aws/credentials -v "${PWD}":/work:rw -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm ansible_from_ubuntu:latest  ansible-playbook $playbook_name $*
-	cp /opt/*.csv /media
+	cd $git_repo_dir
+        cp ~/.ansible_pass .
+	sudo docker run -v "${PWD}":/work:rw --rm ansible_from_ubuntu:latest ansible-vault decrypt /work/.aws/credentials --vault-password-file /work/.ansible_pass
+	sudo docker run --env AWS_SHARED_CREDENTIALS_FILE=/work/.aws/credentials -v "${PWD}":/work:rw -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm ansible_from_ubuntu:latest ansible-playbook $playbook_name $*
+	cp *.csv ../
 	git clean -f -d
 else
 	echo "Error in cloning git repo"
