b3828b362362da240801386ccf91855f8a509fa2
muralisp-acoustic <112857287+muralisp-acoustic@users.noreply.github.com>
2022-12-30 14:39:34 +0000

    encrypt credentials after ansible run

--- run_ansible.sh
+++ run_ansible.sh
@@ -18,6 +18,7 @@ then
         cp ~/.ansible_pass .
 	sudo docker run -v "${PWD}":/work:rw --rm ansible_from_ubuntu:latest ansible-vault decrypt /work/.aws/credentials --vault-password-file /work/.ansible_pass
 	sudo docker run --env AWS_SHARED_CREDENTIALS_FILE=/work/.aws/credentials -v "${PWD}":/work:rw -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm ansible_from_ubuntu:latest ansible-playbook $playbook_name $*
+	sudo docker run -v "${PWD}":/work:rw --rm ansible_from_ubuntu:latest ansible-vault encrypt /work/.aws/credentials --vault-password-file /work/.ansible_pass
 	cp *.csv ../
 	git clean -f -d
 else
