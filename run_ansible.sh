a8398927ad0e275c1e38a7b785b6723cf12a07d6
Ubuntu <ubuntu@mailrelay-adm-ue1.prod.awspr>
2023-01-10 12:13:57 +0000

    fix

--- run_ansible.sh
+++ run_ansible.sh
@@ -18,7 +18,7 @@ then
 	cd $git_repo_dir
         cp ~/.ansible_pass .
 	sudo docker run -v "${PWD}":/work:rw --rm public.ecr.aws/f4q9n3z9/ansible:$version ansible-vault decrypt /work/.aws/credentials --vault-password-file /work/.ansible_pass
-	sudo docker run --env AWS_SHARED_CREDENTIALS_FILE=/work/.aws/credentials -v "${PWD}":/work:rw -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm ansible_from_ubuntu:$version  ansible-playbook $playbook_name $*
+	sudo docker run --env AWS_SHARED_CREDENTIALS_FILE=/work/.aws/credentials -v "${PWD}":/work:rw -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm public.ecr.aws/f4q9n3z9/ansible:$version  ansible-playbook $playbook_name $*
 	sudo docker run -v "${PWD}":/work:rw --rm public.ecr.aws/f4q9n3z9/ansible:$version ansible-vault encrypt /work/.aws/credentials --vault-password-file /work/.ansible_pass
 	cp *.csv ../
 	git clean -f -d
